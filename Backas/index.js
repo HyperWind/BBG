383082918928f754911619da1e2fe9bf9ed21549
Martynas Stankevičius <Martynoemailas@yahoo.com>
2018-10-06 12:51:46 +0300

    Api pradzia

--- Backas/index.js
+++ Backas/index.js
@@ -1,7 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// call the packages we need
+const express    = require('express');        // call express
+const app        = express();                 // define our app using express
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// const port = process.env.PORT || 8080;
+const port = 3000;
+
+var router = express.Router();             
+
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
